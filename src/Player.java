c2f24807f91e8e94bb94736026da5a9a3bf3fae1
alekekex <alexander_khan@dlsu.edu.ph>
2026-08-16 12:20:33 +0800

    initialized class

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,28 @@
-public class Player {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Player {
+    private String name;
+    private List<Piece> pieces;
+
+    public Player(String name, List<Piece> pieces) {
+        this.name = name;
+        this.pieces = pieces;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Piece> getPieces() {
+        return pieces;
+    }
+
+    public void addPiece(Piece piece) {
+        pieces.add(piece);
+    }
+
+    public void removePiece(Piece piece) {
+        pieces.remove(piece);
+    }
 }
