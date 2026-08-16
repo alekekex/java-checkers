e7a1f8e116cfcde8f039d86964168b0877850a4d
alekekex <alexander_khan@dlsu.edu.ph>
2026-08-16 19:20:29 +0800

    added board display method

--- src/Display.java
+++ src/Display.java
@@ -1,3 +1,26 @@
 public class Display {
+    public static void displayBoard(Piece[][] board) {
+        System.out.println();
+        System.out.println("    A   B   C   D   E   F   G   H");
+        System.out.println("  +---+---+---+---+---+---+---+---+");
 
+        for (int i = 7; i >= 0; i--) {
+            System.out.print((i + 1) + " ");
+
+            for (int j = 0; j < 8; j++) {
+                System.out.print("| ");
+
+                if (board[i][j] != null)
+                    System.out.print(board[i][j].getPiece());
+                else System.out.print(" ");
+
+                System.out.print(" ");
+            }
+
+            System.out.println("|");
+            System.out.println("  +---+---+---+---+---+---+---+---+");
+        }
+
+        System.out.println();
+    }
 }
