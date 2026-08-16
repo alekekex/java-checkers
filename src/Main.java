e33a87dcaf1e5b544ddd2d4bd191e02e554eb8a2
alekekex <alexander_khan@dlsu.edu.ph>
2026-08-16 21:38:46 +0800

    initialized main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean isRunning = true;
 
+        while (isRunning) {
+            Display.displayIntro();
+            int choice = Input.getIntChoice(sc, "Enter your choice: ", 1, 3);
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Gameplay will be implemented soon!");
+                    break;
+                case 2:
+                    Display.displayRules();
+                    Input.getEnterInput(sc, "Press Enter to go back...");
+                    break;
+                case 3:
+                    isRunning = false;
+                    Display.displayExit();
+                    break;
+            }
+        }
+
+        sc.close();
     }
 }
