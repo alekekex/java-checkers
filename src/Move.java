f621c6b4544e29ea7a8a7444354b7022930cadd8
alekekex <alexander_khan@dlsu.edu.ph>
2026-08-16 12:25:55 +0800

    initialized class

--- src/Move.java
+++ src/Move.java
@@ -1,3 +1,17 @@
 public class Move {
+    public String start;
+    public String end;
 
+    public Move(String end, String start) {
+        this.end = end;
+        this.start = start;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
 }
