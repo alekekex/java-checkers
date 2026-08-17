7676f31f1476de458054f8ae46cb7d319800fd8b
alekekex <alexander_khan@dlsu.edu.ph>
2026-08-17 11:58:47 +0800

    partially initialized class

--- src/Checkers.java
+++ src/Checkers.java
@@ -1,3 +1,34 @@
+import java.util.Scanner;
+
 public class Checkers {
+    private Board board;
+    private Player[] players;
+    private int playerIdx;
+    private boolean isGameOver;
+
+    public Checkers() {
+        this.board = new Board();
+        this.players = new Player[2];
+        this.playerIdx = 0;
+        this.isGameOver = false;
+    }
+
+    public void playGame(Scanner sc) {
+        initializePlayers(sc);
+
+        while (!isGameOver) {
+            // game stuff
+        }
+    }
+
+    public void initializePlayers(Scanner sc) {
+        Display.displayPlayerSetup();
+        String whiteName = Input.getName(sc, "Enter White player's name: ");
+        String blackName = Input.getName(sc, "Enter Black player's name: ");
+
+        players[0] = new Player(whiteName, board.getWhitePieces());
+        players[1] = new Player(blackName, board.getBlackPieces());
+    }
+
 
 }
