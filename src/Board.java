54cfeb60a39598c2803a902f05a4016da5e541e6
alekekex <alexander_khan@dlsu.edu.ph>
2026-08-18 15:14:02 +0800

    removed lists

--- src/Board.java
+++ src/Board.java
@@ -1,15 +1,8 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Board {
     private Piece[][] board;
-    private List<Piece> whitePieces;
-    private List<Piece> blackPieces;
 
     public Board() {
         this.board = new Piece[8][8];
-        this.whitePieces = new ArrayList<>();
-        this.blackPieces = new ArrayList<>();
 
         initializeBoard();
     }
@@ -18,27 +11,15 @@ public class Board {
         return board;
     }
 
-    public List<Piece> getWhitePieces() {
-        return whitePieces;
-    }
-
-    public List<Piece> getBlackPieces() {
-        return blackPieces;
-    }
-
     public void initializeBoard() {
         for (int i = 7; i >= 0; i--) {
             for (int j = 0; j < 8; j++) {
                 if (i > 4) {
-                    if (i % 2 == j % 2) {
+                    if (i % 2 == j % 2)
                         board[i][j] = new NormalPiece("BLACK");
-                        blackPieces.add(board[i][j]);
-                    }
                 } else if (i < 3) {
-                    if (i % 2 == j % 2) {
+                    if (i % 2 == j % 2)
                         board[i][j] = new NormalPiece("WHITE");
-                        whitePieces.add(board[i][j]);
-                    }
                 }
             }
         }
